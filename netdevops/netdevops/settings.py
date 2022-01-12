0285a67aa0f66e1379eb8a4f42355581a411d104
Nockternal <burger@ru.co.za>
2022-01-12 15:14:34 +0200

    add any hosts to server

--- netdevops/netdevops/settings.py
+++ netdevops/netdevops/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-7!-$jpz*ktm6)+ko8jg=(9b^w#zg*mr-icl-+)4h1!-vbvx0n9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
