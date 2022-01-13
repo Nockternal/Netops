4c322aebc0a6946f3c2de405193e3aaecf9bcff3
Nockternal <burger@ru.co.za>
2022-01-13 13:51:28 +0200

    testing render option in views

--- django/netdevops/network/urls.py
+++ django/netdevops/network/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 
+# sub pages under the network section below is an example of webpage/network/commands page referencing the index function
 urlpatterns = [
     path("commands", views.index, name="index")
 ]
