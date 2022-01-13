cf9f1a2bca86222a70e65a6073fb803819f9f5e9
Nockternal <burger@ru.co.za>
2022-01-13 14:59:24 +0200

    adding console logging

--- django/netdevops/network/views.py
+++ django/netdevops/network/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import logging
+
+"""
+logging.basicConfig(level=logging.INFO) # Here
+logging.debug("Log message goes here.")
+logging.info("Log message goes here.")
+logging.warning("Log message goes here.")
+logging.error("Log message goes here.")
+logging.critical("Log message goes here.")
+"""
 
 months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
 # Create your views here.
