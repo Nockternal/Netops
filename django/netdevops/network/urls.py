1878d5174b51b2b860c9608aad167779a2a7f2bf
Nockternal <burger@ru.co.za>
2022-01-13 14:51:25 +0200

    troublshoot what incomming request is

--- django/netdevops/network/urls.py
+++ django/netdevops/network/urls.py
@@ -4,5 +4,5 @@ from . import views
 # sub pages under the network section below is an example of webpage/network/commands page referencing the index function
 urlpatterns = [
     path("<str:month>", views.monthly, name="monthly"),
-    path("<int:month>", views.monthlyNumbers, name="monthly"),
+    path("<int:month>", views.monthlyNumbers, name="monthlyNumbers"),
 ]
