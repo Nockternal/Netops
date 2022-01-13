c88d33c82532fd0cf363a473278bba28c6aaf174
Nockternal <burger@ru.co.za>
2022-01-13 15:07:43 +0200

    test2

--- django/netdevops/network/urls.py
+++ django/netdevops/network/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 # sub pages under the network section below is an example of webpage/network/commands page referencing the index function
 urlpatterns = [
-    path("<str:month>", views.monthly, name="monthly"),
     path("<int:month>", views.monthlyNumbers, name="monthlyNumbers"),
+    path("<str:month>", views.monthly, name="monthly")
 ]
