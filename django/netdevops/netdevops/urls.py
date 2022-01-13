c9f808a7c437a6b7240e87406701b4ea5e49863a
Nockternal <burger@ru.co.za>
2022-01-13 13:30:31 +0200

    added include into main ulrs.py

--- django/netdevops/netdevops/urls.py
+++ django/netdevops/netdevops/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 
 # path('network/', include('network.urls')), network.urls where network is the app name/ folder and urls is the urls.py file in that folder
