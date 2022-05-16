3c55be2da9734e4e32a8f2e351b1e7b0c81d56a0
Nockternal <burger@ru.co.za>
2022-05-16 11:11:24 +0200

    update

--- react/first django/backend/backend/settings.py
+++ react/first django/backend/backend/settings.py
@@ -8,6 +8,9 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
 from pathlib import Path
 
@@ -19,7 +22,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-e6&+k)zh+ul!0(tvwditdzh=^%*cwgao4cvsy6&vx$reuf)dt4'
+#SECRET_KEY = 'django-insecure-e6&+k)zh+ul!0(tvwditdzh=^%*cwgao4cvsy6&vx$reuf)dt4'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
